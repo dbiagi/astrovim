6bf36974e9f3ffb51ef4922ba2bbbf7dbeacc2eb
Diego de Biagi <diegobiagi@protonmail.com>
2026-07-14 22:45:31 -0300

    feat: language packs

--- lua/community.lua
+++ lua/community.lua
@@ -5,12 +5,35 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- Language Packs
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.kotlin" },
   { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.spring-boot" },
   { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.biome" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.golangci-lint" },
+  { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.xml" },
+
+  -- Colorschemes
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.gruvbox-baby" },
-  -- { import = "astrocommunity.lsp.coc-nvim" },
+
+  -- Markdown
   { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
+
+  -- Project Manager
+  { import = "astrocommunity.project.projectmgr-nvim" },
+
+  -- Unused functions/variables
+  { import = "astrocommunity.utility.neodim" },
 }
